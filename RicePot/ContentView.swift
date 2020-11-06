bfca5e2d39ef81411aedaed43819725fd30f082d
den0206 <mac.pop@mineo.jp>
2020-11-06 20:41:02 +0900

    Initial Commit

---
+++ RicePot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RicePot
+//
+//  Created by 酒井ゆうき on 2020/11/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RicePot/RicePotApp.swift
@@ -0,0 +1,17 @@
+//
+//  RicePotApp.swift
+//  RicePot
+//
+//  Created by 酒井ゆうき on 2020/11/06.
+//
+
+import SwiftUI
+
+@main
+struct RicePotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
